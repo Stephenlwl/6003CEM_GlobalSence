73281d4ed421d8576d81ae603dce4ff85d1a4ce6
stephen <stephen@globalconnect.com.my>
2025-05-26 01:25:11 +0800

    change the iconic picture  wikipedia api to pixabay api , and add additional weather information

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function Weather() {
   const [loading, setLoading] = useState(false);
   const [image, setImage] = useState(null); //declare the image to display related background image
 
-
   const countries = Country.getAllCountries();
   const states = State.getStatesOfCountry(country);
 
-  const APIKEY = "33859b42938f41308af85015250205";
+  const APIKEY_WEATHER = "33859b42938f41308af85015250205";
+  const APIKEY_ICONIC = "35193368-4e77f65738df9f6044ab18420";
   const FORECAST_DAYS = 3; // number of days for forecast
   const fetchWeatherData = async () => {
     setHasSearched(true);
@@ -42,18 +42,26 @@ function Weather() {
 
     const query = `${selectedState.name},${selectedCountry.name}`;
     const queryState = selectedState.name;
-    const current_weather_api = `http://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${query}&aqi=no`;
-    const forecas_api = `http://api.weatherapi.com/v1/forecast.json?key=${APIKEY}&q=${query}&days=${FORECAST_DAYS}&aqi=no&alerts=yes`;
-    const weather_alert_api = `http://api.weatherapi.com/v1/alerts.json?key=${APIKEY}&q=${query}`;
+    const current_weather_api = `http://api.weatherapi.com/v1/current.json?key=${APIKEY_WEATHER}&q=${query}&aqi=no`;
+    const forecas_api = `http://api.weatherapi.com/v1/forecast.json?key=${APIKEY_WEATHER}&q=${query}&days=${FORECAST_DAYS}&aqi=no&alerts=yes`;
+    const weather_alert_api = `http://api.weatherapi.com/v1/alerts.json?key=${APIKEY_WEATHER}&q=${query}`;
 
-    const country_state_iconic_api = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${queryState}`);
-    
-    const image = await country_state_iconic_api.json();
-      if (image.originalimage?.source) {
-        setImage(image.originalimage.source); // Set the image from the API response
+    // const country_state_iconic_api = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${queryState}`);
+    const country_state_iconic_api = await fetch(`https://pixabay.com/api/?key=${APIKEY_ICONIC}&q=${queryState}&category=places&image_type=photo`);
+
+
+    try {
+      const image = await country_state_iconic_api.json();
+      if (image.hits && image.hits.length > 0) {
+        setImage(image.hits[0].largeImageURL); // set the image from the API response
       } else {
-        setImage(null); // No result
+        setImage(null);
       }
+    } catch (error) {
+      console.error("Error fetching image:", error);
+      setImage(null);
+    }
+
 
     try {
       const currentRes = await fetch(current_weather_api);
@@ -110,7 +118,7 @@ function Weather() {
           </button>
         </div>
       </div>
-      
+
       {!hasSearched && (
         <div className="alert alert-info">
           <strong>Note:</strong><br></br>
@@ -124,45 +132,107 @@ function Weather() {
           </div>
           <p className="mt-2">Please be patient, We are getting related weather info for you...</p>
         </div>
-      )}  
-      {/* display the selected country's state current weather info from api */}
-      {weatherData && (
-        <div className="card mb-4">
-          {image ? (
-              <img src={image} alt="The iconic place of the state" className="card-img-top" style={{height: 'auto', width: '250px;', objectFit: 'cover'}} />
-            ) : (
-              <img src="https://via.placeholder.com/600x300?text=No+Image+Found" alt="No image found" className="card-img-top" />
-            )}
-          <div className="card-body text-center">
-            <h4>Current Weather in {weatherData.location.name}, {weatherData.location.country}</h4>
-            <img src={weatherData.current.condition.icon} alt="Icon" />
-            <p>{weatherData.current.condition.text}</p>
-            <p><strong>Temperature:</strong> {weatherData.current.temp_c}°C</p>
-            <p><strong>Humidity:</strong> {weatherData.current.humidity}%</p>
-            <p><strong>Wind:</strong> {weatherData.current.wind_kph} kph</p>
-            <p><strong>Last Updated:</strong> {weatherData.current.last_updated}</p>
-          </div>
-          <div>
-            {alertData?.alerts?.alert.length < 1 ? (
-              <p className="text-success">No weather alerts.</p>
-            ) : (
-              <>
-                <p className="text-danger">Weather Alerts:</p>
-                {alertData && (
-                  <div className="mt-3 alert alert-danger">
-                    <p><strong>Effective Date: </strong>{alertData.alerts.alert[0].effective}</p>
-                    <p><strong>Event: </strong>{alertData.alerts.alert[0].event}</p>
-                    <p><strong>Severity: </strong>{alertData.alerts.alert[0].severity}</p>
-                    <p><strong>Area: </strong>{alertData.alerts.alert[0].areas}</p>
-                    <p><strong>Description: </strong>{alertData.alerts.alert[0].desc}</p>
-                    <p><strong>Instructions: </strong>{alertData.alerts.alert[0].instruction}</p>
-                  </div>
-                )}
-              </>
-            )}
-          </div>
-        </div>
       )}
+      {/* display the selected country's state current weather info from api */}
+      <div className="row">
+        <div className="col-md-8 text-center">
+          {weatherData && (
+            <div
+              className="card mb-4 text-white weather-section"
+              style={{
+                // the url of placeholder image is to indicate user when the image is not found
+                backgroundImage: `url(${image || "https://placehold.co/600x400/beige/white?text=No+Iconic+Image+Found"})`,
+                backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', height: '500px'
+              }}
+            >
+              <div className="card-body text-center bg-dark bg-opacity-50 rounded">
+                <h4>Current Weather in {weatherData.location.name}, {weatherData.location.country}</h4>
+                <img src={weatherData.current.condition.icon} alt="Icon" />
+                <p>{weatherData.current.condition.text}</p>
+                <p><strong>Temperature:</strong> {weatherData.current.temp_c ?? 'N/A'}°C</p>
+                <p><strong>Humidity:</strong> {weatherData.current.humidity ?? 'N/A'}%</p>
+                <p><strong>Wind:</strong> {weatherData.current.wind_kph ?? 'N/A'} kph</p>
+                <p><strong>Last Updated:</strong> {weatherData.current.last_updated ?? 'N/A'}</p>
+              </div>
+            </div>
+          )}
+          {alertData?.alerts?.alert.length < 1 ? (
+            <div className="mt-3 alert alert-info">
+              <p className="text-success">No Weather Alerts For Today in This State.</p>
+            </div>
+          ) : (
+            <>
+
+              {alertData && (
+                <div className="mt-3 alert alert-danger text-start">
+                  <p className="text-danger">Weather Alerts:</p>
+                  <p><strong>Effective Date: </strong>{alertData.alerts.alert[0].effective ?? 'N/A'}</p>
+                  <p><strong>Event: </strong>{alertData.alerts.alert[0].event ?? 'N/A'}</p>
+                  <p><strong>Severity: </strong>{alertData.alerts.alert[0].severity ?? 'N/A'}</p>
+                  <p><strong>Area: </strong>{alertData.alerts.alert[0].areas ?? 'N/A'}</p>
+                  <p><strong>Description: </strong>{alertData.alerts.alert[0].desc ?? 'N/A'}</p>
+                  <p><strong>Instructions: </strong>{alertData.alerts.alert[0].instruction ?? 'N/A'}</p>
+                </div>
+              )}
+            </>
+          )}
+        </div>
+        {weatherData && (
+          <div className="col-md-4">
+            <div className="card text-dark mb-4 shadow-sm">
+              <div className="card-body">
+                <h5 className="card-title text-center mb-4">Additional Weather Details</h5>
+
+                {/* pressure, visibility, humidity */}
+                <div className="row text-center mb-3">
+                  <div className="col-md-4 border-end">
+                    <p className="mb-1"><strong>Pressure</strong></p>
+                    <p>{weatherData.current.pressure_mb ?? 'N/A'} mb</p>
+                  </div>
+                  <div className="col-md-4 border-end">
+                    <p className="mb-1"><strong>Visibility</strong></p>
+                    <p>{weatherData.current.vis_km ?? 'N/A'} km</p>
+                  </div>
+                  <div className="col-md-4">
+                    <p className="mb-1"><strong>Humidity</strong></p>
+                    <p>{weatherData.current.humidity ?? 'N/A '}%</p>
+                  </div>
+                </div>
+
+                {/* wind */}
+                <div className="row text-center mb-3">
+                  <div className="col-md-12">
+                    <p className="mb-1"><strong>Wind</strong></p>
+                    <p>{weatherData.current.wind_dir} at {weatherData.current.wind_kph} kph</p>
+                  </div>
+                </div>
+
+                {/* time-based temperature */}
+                <h6 className="text-center mt-4">Temperature by Time of Day</h6>
+                <div className="row text-center">
+                  <div className="col-md-3 border-end">
+                    <p className="mb-1"><strong>Morning</strong></p>
+                    <p>{forecastData?.forecast?.forecastday[0]?.hour[8]?.temp_c ?? 'N/A'}°C</p>
+                  </div>
+                  <div className="col-md-3 border-end">
+                    <p className="mb-1"><strong>Afternoon</strong></p>
+                    <p>{forecastData?.forecast?.forecastday[0]?.hour[14]?.temp_c ?? 'N/A'}°C</p>
+                  </div>
+                  <div className="col-md-3 border-end">
+                    <p className="mb-1"><strong>Evening</strong></p>
+                    <p>{forecastData?.forecast?.forecastday[0]?.hour[18]?.temp_c ?? 'N/A'}°C</p>
+                  </div>
+                  <div className="col-md-3">
+                    <p className="mb-1"><strong>Night</strong></p>
+                    <p>{forecastData?.forecast?.forecastday[0]?.hour[22]?.temp_c ?? 'N/A'}°C</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+
 
       {/* the selected country's forecast data from api */}
       {forecastData?.forecast?.forecastday && (
